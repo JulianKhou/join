2ef9614ce8b9e1570357009494d74ac07c8f6641
Maurice Stemmert <maurice.stemmert@gmail.com>
2026-06-05 11:10:49 +0200

    Update loadingAnimation.js

--- js/loadingAnimation.js
+++ js/loadingAnimation.js
@@ -1,7 +1,5 @@
 const loader = document.getElementById("loader");
 
-// Die Logo-Animation soll bei jedem Öffnen von LogIn.html abspielen,
-// daher wird der zuvor gesetzte Merker (vorerst) ignoriert.
 loader.addEventListener("animationend", (event) => {
   if (event.animationName === "loaderFadeOut") {
     loader.style.display = "none";
